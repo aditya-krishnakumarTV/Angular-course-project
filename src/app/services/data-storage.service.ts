c50134a7db4b2ad2d296b9d9add3fa0a139141b9
aditya_kk <adityakrishnakumar12@gmail.com>
2022-10-29 17:49:44 +0530

    Handled a future issue with fetching ingre

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
 
 import { Recipe } from "../shared/recipe.model";
 
@@ -17,15 +18,22 @@ export class DataStorageService {
 
     storeRecipies() {
         const recipes = this.recipeService.getRecipes()
-        this.http.put('https://angular-course-project-176a4-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json', recipes).subscribe(value => {
-            console.log(value)
-        })
+        this.http.put('https://angular-course-project-176a4-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json', recipes)
+            .subscribe(value => {
+                console.log(value)
+            })
     }
 
     fetchRecipes() {
-        this.http.get<Array<Recipe>>('https://angular-course-project-176a4-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json').subscribe(value => {
-            this.recipeService.setRecipes(value)
-        })
+        this.http.get<Array<Recipe>>('https://angular-course-project-176a4-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredient: recipe.ingredient ? recipe.ingredient : [] }
+                })
+            }))
+            .subscribe(value => {
+                this.recipeService.setRecipes(value)
+            })
     }
 
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -15,28 +15,30 @@ export class RecipeService {
 
     constructor(private shoppingListService: ShoppingListService) { }
 
-    private recipe: Array<Recipe> = [
-        new Recipe('Panner Curry With Rice',
-            'This is a rice dish topped with a delicious panner curry!',
-            'https://www.thespruceeats.com/thmb/yK8psUDvXdEKOvzFtLtx-n4ETuQ=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/vegan-tofu-tikka-masala-recipe-3378484-hero-01-d676687a7b0a4640a55be669cba73095.jpg',
-            [
-                new Ingredient('Panner', 8),
-                new Ingredient('Rice', 5),
-                new Ingredient('Tomatoes', 4),
-                new Ingredient('Corriander', 2),
-            ]
-        ),
-        new Recipe('Butter Chicken With Rice',
-            'This is a rice dish topped with the famous Indian butter chicken!',
-            'https://www.simplyrecipes.com/thmb/1SXZ_F1GC6ww_ppWnrdbKgHi9fQ=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg',
-            [
-                new Ingredient('Chicken', 8),
-                new Ingredient('Rice', 5),
-                new Ingredient('Tomatoes', 4),
-                new Ingredient('Corriander', 2),
-            ]
-        )
-    ]
+    // private recipe: Array<Recipe> = [
+    //     new Recipe('Panner Curry With Rice',
+    //         'This is a rice dish topped with a delicious panner curry!',
+    //         'https://www.thespruceeats.com/thmb/yK8psUDvXdEKOvzFtLtx-n4ETuQ=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/vegan-tofu-tikka-masala-recipe-3378484-hero-01-d676687a7b0a4640a55be669cba73095.jpg',
+    //         [
+    //             new Ingredient('Panner', 8),
+    //             new Ingredient('Rice', 5),
+    //             new Ingredient('Tomatoes', 4),
+    //             new Ingredient('Corriander', 2),
+    //         ]
+    //     ),
+    //     new Recipe('Butter Chicken With Rice',
+    //         'This is a rice dish topped with the famous Indian butter chicken!',
+    //         'https://www.simplyrecipes.com/thmb/1SXZ_F1GC6ww_ppWnrdbKgHi9fQ=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg',
+    //         [
+    //             new Ingredient('Chicken', 8),
+    //             new Ingredient('Rice', 5),
+    //             new Ingredient('Tomatoes', 4),
+    //             new Ingredient('Corriander', 2),
+    //         ]
+    //     )
+    // ]
+
+    private recipe: Array<Recipe> = []
 
     setRecipes(recipes: Recipe[]) {
         this.recipe = recipes
